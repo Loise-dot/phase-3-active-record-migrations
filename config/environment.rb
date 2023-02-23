89b2f83a9e6f410b2f05ff9ca82f608ebe18c448
Loise <loise.wambui@student.moringaschool.com>
2023-02-23 18:20:29 +0300

    Done

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,5 @@
+#ENV["RACK_ENV"] =>environment variable determines  whether our code is running in development environment or test environment
+#"RACK_ENV" => environment used by sinatra-activerecord to determine which database to connect to in our env file. (development database)
 ENV["RACK_ENV"] ||= "development"
 
 require 'bundler/setup'
